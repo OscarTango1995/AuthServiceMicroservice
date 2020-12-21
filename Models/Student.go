6dcb88874a0c9ebe280e4a35b6ed6a7ca9d797bd
Ovais Tariq <ovais.tariq@yahoo.com>
2020-12-21 12:09:55 +0500

    Models Created

--- Models/Student.go
+++ Models/Student.go
@@ -1,1 +1,9 @@
 package Models
+
+type Student struct {
+	id int
+	name string
+	semester int
+	department string
+	current_grade string
+}
--- Models/Teacher.go
+++ Models/Teacher.go
@@ -1,1 +1,11 @@
 package Models
+
+type Teacher struct {
+	id int
+	name string
+	qualification string
+	designation string
+	department string
+	date_of_joining string
+}
+
